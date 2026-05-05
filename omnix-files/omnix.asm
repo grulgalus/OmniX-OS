efe7e8c6dc70fd56ecd66bf0731da53fa53f91c2
grulgalus <nee017310@gmail.com>
2026-05-05 15:01:07 +0200

    Přidána podpora klávesnice do OmniX OS

--- omnix-files/omnix.asm
+++ omnix-files/omnix.asm
@@ -1,5 +1,5 @@
-org 0x7C00        ; Přesně sem BIOS načítá operační systém
-bits 16           ; Jsme ve starém dobrém 16-bitovém režimu
+org 0x7C00
+bits 16
 
 start:
     ; Příprava paměti
@@ -9,22 +9,30 @@ start:
     mov es, ax
     sti
 
-    ; Načtení textu
+    ; Vypsání uvítacího textu
     mov si, message
 
 print_char:
-    lodsb             ; Načte jedno písmeno do AL
-    cmp al, 0         ; Je to konec textu (nula)?
-    je done           ; Pokud ano, přeskoč na konec
-    mov ah, 0x0E      ; BIOS funkce: Vypiš znak na obrazovku
-    int 0x10          ; Zavolej BIOS
-    jmp print_char    ; Opakuj pro další písmeno
+    lodsb             ; Načte písmeno
+    cmp al, 0         ; Je to konec textu?
+    je keyboard_loop  ; Pokud ano, skoč na čtení klávesnice!
+    mov ah, 0x0E      ; BIOS funkce pro výpis
+    int 0x10
+    jmp print_char
 
-done:
-    hlt               ; Uspi procesor
-    jmp done          ; Zaseknutí v nekonečné smyčce (aby se to nevyplo)
+keyboard_loop:
+    ; Čekání na stisk klávesy (BIOS funkce 0x16)
+    mov ah, 0x00
+    int 0x16          ; Klávesa se uloží do registru AL
 
-message db 'Vitejte v OmniX OS! (Verze 0.1, Vlastni Bootloader)', 0
+    ; Vypsání stisknuté klávesy na obrazovku (BIOS funkce 0x10)
+    mov ah, 0x0E
+    int 0x10
+
+    ; Zpět na čekání na další klávesu
+    jmp keyboard_loop
+
+message db 'Vitejte v OmniX OS! Zkuste neco napsat: ', 0
 
 times 510-($-$$) db 0 ; Zbytek paměti do 512 bytů vyplníme nulami
-dw 0xAA55             ; MAGICKÝ PODPIS! Tohle řekne Limbu: "Jsem bootovací disk!"
+dw 0xAA55             ; Magický podpis bootovacího sektoru
