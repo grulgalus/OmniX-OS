64a4fb211edd6ca2992b1c1eb00d5b46c4f4055d
grulgalus <nee017310@gmail.com>
2026-05-07 09:45:57 +0200

    Opravení chyb v bootloaderu

--- omnix-files/bootloader/asm/boot.asm
+++ omnix-files/bootloader/asm/boot.asm
@@ -1,6 +1,5 @@
 org 0x7C00
 bits 16
-
 start:
     cli
     xor ax, ax
@@ -9,18 +8,15 @@ start:
     mov ss, ax
     mov sp, 0x7C00
     sti
-
     in al, 0x92
     or al, 2
     out 0x92, al
-
     cli
     lgdt [gdt_descriptor]
     mov eax, cr0
     or eax, 0x1
     mov cr0, eax
     jmp 0x08:start32
-
 align 4
 gdt_start:
     dq 0x0
@@ -29,11 +25,9 @@ gdt_code:
 gdt_data:
     dw 0xFFFF, 0x0000, 0x9200, 0x00CF
 gdt_end:
-
 gdt_descriptor:
     dw gdt_end - gdt_start - 1
     dd gdt_start
-
 bits 32
 start32:
     mov ax, 0x10
@@ -43,7 +37,7 @@ start32:
     mov gs, ax
     mov ss, ax
     mov esp, 0x90000
+    mov dword [0xB8000], 0x0F4B0F4F
     jmp 0x7E00
-
 times 510-($-$$) db 0
 dw 0xAA55
