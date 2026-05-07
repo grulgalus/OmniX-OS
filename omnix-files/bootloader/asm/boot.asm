02009dd082561ac53597a2041ad07e505b2bde69
grulgalus <nee017310@gmail.com>
2026-05-07 10:09:44 +0200

    Ahh

--- omnix-files/bootloader/asm/boot.asm
+++ omnix-files/bootloader/asm/boot.asm
@@ -2,9 +2,6 @@ org 0x7C00
 bits 16
 
 start:
-    jmp 0x0000:normalize_cs
-
-normalize_cs:
     cli
     xor ax, ax
     mov ds, ax
@@ -17,16 +14,34 @@ normalize_cs:
     or al, 2
     out 0x92, al
 
-    mov si, msg
-.print:
-    lodsb
-    test al, al
-    jz .pmode
-    mov ah, 0x0E
-    int 0x10
-    jmp .print
+    mov si, msg_prompt
+    call print_str
 
-.pmode:
+wait_key:
+    mov ah, 0x00
+    int 0x16         
+    
+    cmp al, 0x04     ; CTRL+D
+    je show_debug
+    
+    cmp al, 0x0D     ; ENTER
+    je boot_kernel
+    
+    jmp wait_key
+
+show_debug:
+    mov si, msg_debug
+    call print_str
+    
+wait_debug_key:
+    mov ah, 0x00
+    int 0x16
+    jmp boot_kernel
+
+boot_kernel:
+    mov si, msg_booting
+    call print_str
+
     cli
     lgdt [gdt_descriptor]
     mov eax, cr0
@@ -34,7 +49,20 @@ normalize_cs:
     mov cr0, eax
     jmp 0x08:start32
 
-msg db "OmniX Boot...", 13, 10, 0
+print_str:
+    mov ah, 0x0E
+.loop:
+    lodsb
+    test al, al
+    jz .done
+    int 0x10
+    jmp .loop
+.done:
+    ret
+
+msg_prompt  db "OmniX OS Zavadec", 13, 10, "-> ENTER: Start OS", 13, 10, "-> CTRL+D: Debug", 13, 10, 0
+msg_debug   db 13, 10, "=== DEBUG ===", 13, 10, "[OK] Bootloader na 0x7C00", 13, 10, "Stiskni libovolnou klavesu...", 13, 10, 0
+msg_booting db 13, 10, "Spoustim OmniX Kernel (0x7E00)...", 13, 10, 0
 
 align 4
 gdt_start:
