a04f70a397de9e17cdec2d8ea82e4694cbb4200a
grulgalus <nee017310@gmail.com>
2026-05-08 14:47:27 +0200

    test

--- omnix-files/kernel/rust/main.rs
+++ omnix-files/kernel/rust/main.rs
@@ -1,5 +1,5 @@
-#![no_std]
-#![no_main]
+// #![no_std]
+// #![no_main]
 
 #[path = "../../drivers/rust/vga.rs"]
 pub mod vga;
@@ -16,38 +16,52 @@ pub mod installer;
 pub mod system_ui;
 pub mod omxapk;
 
-use core::panic::PanicInfo;
+//use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! { 
-    loop { unsafe { core::arch::asm!("hlt"); } } 
-}
+//#[panic_handler]
+//fn panic(_info: &PanicInfo) -> ! { 
+//    loop { unsafe { core::arch::asm!("hlt"); } } 
+//}
 
-#[link_section = ".text._start"]
+//#[link_section = ".text._start"]
+//#[no_mangle]
+//pub extern "C" fn _start() -> ! {
+//    installer::run_installer();
+//    loop { unsafe { core::arch::asm!("hlt"); } }
+//}
+
+//#[no_mangle]
+//pub unsafe extern "C" fn memset(s: *mut u8, c: i32, n: usize) -> *mut u8 {
+//    let mut i = 0; while i < n { *s.add(i) = c as u8; i += 1; } s
+//}
+//#[no_mangle]
+//pub unsafe extern "C" fn memcpy(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
+//    let mut i = 0; while i < n { *dest.add(i) = *src.add(i); i += 1; } dest
+//}
+//#[no_mangle]
+//pub unsafe extern "C" fn memmove(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
+//    if dest < src as *mut u8 { memcpy(dest, src, n) } else {
+//        let mut i = n; while i > 0 { i -= 1; *dest.add(i) = *src.add(i); } dest
+//    }
+//}
+//#[no_mangle]
+//pub unsafe extern "C" fn memcmp(s1: *const u8, s2: *const u8, n: usize) -> i32 {
+//    let mut i = 0; while i < n {
+//        let a = *s1.add(i); let b = *s2.add(i);
+//        if a != b { return a as i32 - b as i32; } i += 1;
+//    } 0
+//}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    installer::run_installer();
-    loop { unsafe { core::arch::asm!("hlt"); } }
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn memset(s: *mut u8, c: i32, n: usize) -> *mut u8 {
-    let mut i = 0; while i < n { *s.add(i) = c as u8; i += 1; } s
-}
-#[no_mangle]
-pub unsafe extern "C" fn memcpy(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
-    let mut i = 0; while i < n { *dest.add(i) = *src.add(i); i += 1; } dest
-}
-#[no_mangle]
-pub unsafe extern "C" fn memmove(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
-    if dest < src as *mut u8 { memcpy(dest, src, n) } else {
-        let mut i = n; while i > 0 { i -= 1; *dest.add(i) = *src.add(i); } dest
+    // NATVRDO PŘEPÍŠEME CELOU PAMĚŤ NA ČERVENOU BARVU
+    let vga_pamet = 0xA0000 as *mut u8;
+    
+    unsafe {
+        for i in 0..64000 {
+            *vga_pamet.add(i) = 4; // 4 = Červená barva ve VGA
+        }
     }
-}
-#[no_mangle]
-pub unsafe extern "C" fn memcmp(s1: *const u8, s2: *const u8, n: usize) -> i32 {
-    let mut i = 0; while i < n {
-        let a = *s1.add(i); let b = *s2.add(i);
-        if a != b { return a as i32 - b as i32; } i += 1;
-    } 0
+
+    loop {} // Zastavíme procesor
 }
